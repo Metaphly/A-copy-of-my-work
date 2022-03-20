24a07e4e7aea581ab593c25e86f3af96a747fdb0
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-03-20 05:57:25 +0000

    Sun, Mar 20, 2022, 5:57 AM +00:00

--- wdc/prac3/task3.js
+++ wdc/prac3/task3.js
@@ -19,12 +19,12 @@ function make_post()
 
 function hide_main()
 {
-    document.getElementById("main").style.display = "none";
+    document.getElementById("main").style.display = "hidden";
     document.getElementById("menu").style.display = "block";
 }
 
 function hide_menu()
 {
     document.getElementById("main").style.display = "block";
-    document.getElementById("menu").style.display = "none";
+    document.getElementById("menu").style.display = "hidden";
 }
