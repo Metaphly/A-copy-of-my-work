c25b715159f24b067b6e1352e5df854855a94bff
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-03-26 05:48:06 +0000

    Sat, Mar 26, 2022, 5:48 AM +00:00

--- wdc/prac4/routes/index.js
+++ wdc/prac4/routes/index.js
@@ -10,7 +10,8 @@ router.get('/last.txt', function(req, res, next) {
 });
 
 router.get('/color.html', function(req, res, next) {
-  
+  let head_co = document.createElement("h1");
+  head_co.innerText = ""
   res.send('<h1>Red</h1>');
 });
 
