7209b54a35dc1a40de6193cb369f406ceb568fe8
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-03-26 03:42:19 +0000

    Sat, Mar 26, 2022, 3:42 AM +00:00

--- wdc/prac4/routes/index.js
+++ wdc/prac4/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/last.txt', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
 module.exports = router;
