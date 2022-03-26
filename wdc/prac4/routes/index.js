ef0ee04bf523104ea11573e4c0e21050fcc51c9f
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-03-26 04:14:26 +0000

    Sat, Mar 26, 2022, 4:14 AM +00:00

--- wdc/prac4/routes/index.js
+++ wdc/prac4/routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-let last_time = "hello";
+let last_time = "";
 
 router.get('/last.txt', function(req, res, next) {
   res.send(last_time);
+  last_time = 
 });
 
 module.exports = router;
