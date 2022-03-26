f157a32f9288612e381294b7fce3a9e1e8a68726
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-03-26 13:56:40 +0000

    Sat, Mar 26, 2022, 1:56 PM +00:00

--- wdc/prac4/public/javascripts/page.js
+++ wdc/prac4/public/javascripts/page.js
@@ -30,9 +30,12 @@ function show_times()
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        var liste = document.createElement("li");
-        liste.innerText = element;
-        thelist.appendChild(liste);
+
+        for (var element of times) {
+            var liste = document.createElement("li");
+            liste.innerText = element;
+            thelist.appendChild(liste);
+            }
         }
     };
     xhttp.open("GET", "/log.json");
