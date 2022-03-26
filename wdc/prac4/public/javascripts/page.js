9bd37fa054cc76fa496b3539771226532c8700e4
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-03-26 14:24:26 +0000

    Sat, Mar 26, 2022, 2:24 PM +00:00

--- wdc/prac4/public/javascripts/page.js
+++ wdc/prac4/public/javascripts/page.js
@@ -29,7 +29,7 @@ function show_times()
 {
     let xhttp = new XMLHttpRequest();
     var tlist = document.getElementById("timel");
-    console.log(tlist.innerHTML);
+    console.log(tlist.innerText);
     tlist.innerHTML="";
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -38,7 +38,7 @@ function show_times()
             var liste = document.createElement("li");
             liste.innerText = times[i];
             console.log(liste.innerText);
-            tlist.appendChild(liste);
+            //tlist.appendChild(liste);
             }
         }
     };
