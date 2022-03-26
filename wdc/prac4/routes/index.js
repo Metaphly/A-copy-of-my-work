ce09103dc99663bdbb8921b5851c6b009661bb6f
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-03-26 07:11:15 +0000

    Sat, Mar 26, 2022, 7:11 AM +00:00

--- wdc/prac4/routes/index.js
+++ wdc/prac4/routes/index.js
@@ -11,21 +11,23 @@ router.get('/last.txt', function(req, res, next) {
 });
 
 router.get('/color.html', function(req, res, next) {
-  /*if(visited%4 == 0)
+  if(visited%4 == 0)
   {
-    res.send('<h1 style="color:red;">red</h1>');
+    //res.send('<h1 style="color:red;">red</h1>');
+    res.sendFile('color1.html', {root: __dirname })
   }else if(visited%4 == 1)
   {
-    res.send('<h1 style="color:yellow;">yellow</h1>');
+    //res.send('<h1 style="color:yellow;">yellow</h1>');
+    res.sendFile('color2.html', {root: __dirname })
   }else if(visited%4 == 2)
   {
-    res.send('<h1 style="color:green;">green</h1>');
+    //res.send('<h1 style="color:green;">green</h1>');
+    res.sendFile('color3.html', {root: __dirname })
   }else if(visited%4 == 3)
   {
-    res.send('<h1 style="color:blue;">blue</h1>');
-  }*/
-
-  res.sendFile('color.html', {root: __dirname })
+    //res.send('<h1 style="color:blue;">blue</h1>');
+    res.sendFile('color4.html', {root: __dirname })
+  }
   visited++;
 });
 
