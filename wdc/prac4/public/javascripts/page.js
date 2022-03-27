37578861419f1b2b940f0c03560cd4addaf69fbf
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-03-27 03:08:46 +0000

    Sun, Mar 27, 2022, 3:08 AM +00:00

--- wdc/prac4/public/javascripts/page.js
+++ wdc/prac4/public/javascripts/page.js
@@ -65,12 +65,13 @@ function update_times()
     xhttp.send();
 }
 
-function check_accept())
+function check_accept()
 {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         xhttp.responseText;
+        console.log(xhttp.responseText);
         }
     };
     xhttp.open("GET", "/content.ajax");
