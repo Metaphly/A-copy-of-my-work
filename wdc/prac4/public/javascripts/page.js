8b7bdc56a400958b3cc6633ade408978f90c073b
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-03-27 03:22:28 +0000

    Sun, Mar 27, 2022, 3:22 AM +00:00

--- wdc/prac4/public/javascripts/page.js
+++ wdc/prac4/public/javascripts/page.js
@@ -68,7 +68,6 @@ function update_times()
 function check_accept()
 {
     let xhttp = new XMLHttpRequest();
-    console.log(this.responseText);
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         xhttp.responseText;
