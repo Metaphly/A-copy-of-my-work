183752e45b7405e70c4b792da3de2c3df5eb64e4
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-04-20 05:37:31 +0000

    Wed, Apr 20, 2022, 5:37 AM +00:00

--- wdc/prac5/query3-5.sql
+++ wdc/prac5/query3-5.sql
@@ -2,4 +2,4 @@ SELECT given_name, family_name, Enrolments.mark
 FROM Students
 INNER JOIN Enrolments
 ON Students.student_id = Enrolments.student_id
-WHERE Enrolments.subject_code='COMP SCI 2207' AND Enrolments.mark<50;
+WHERE Enrolments.subject_code='COMP SCI 2000' AND Enrolments.mark<50;
