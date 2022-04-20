f886d072761dde2788e2498f0e40cd48f97108f7
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-04-20 04:12:52 +0000

    Wed, Apr 20, 2022, 4:12 AM +00:00

--- wdc/prac5/enrolment.sql
+++ wdc/prac5/enrolment.sql
@@ -1,24 +1,24 @@
 CREATE TABLE Students
-{
+(
     student_id VARCHAR(10),
     given_name VARCHAR(20),
     family_name VARCHAR(20),
     program VARCHAR(20),
     PRIMARY KEY (student_id)
-};
+);
 
 CREATE TABLE Subjects
-{
+(
     subject_code VARCHAR(10),
     subject VARCHAR(20),
     faculty VARCHAR(20),
     PRIMARY KEY (subject_code)
-};
+);
 
 CREATE TABLE Enrolments
-{
+(
     student_id VARCHAR(10),
     subject_code VARCHAR(10),
     mark INT,
     PRIMARY KEY (student_id,subject_code)
-};
+);
