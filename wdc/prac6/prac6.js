e81a88660a1c55bf54dffdbfeb75f81a05ae0e6a
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-04-30 08:34:13 +0000

    Sat, Apr 30, 2022, 8:34 AM +00:00

--- wdc/prac6/prac6.js
+++ wdc/prac6/prac6.js
@@ -47,6 +47,7 @@ var vueinst = new Vue({
         choose: 'Choose ...',
         special: SPECIALS[0],
         show_ad: true,
-        dark_mode: false
+        dark_mode: false,
+        back_color
     }
 });
