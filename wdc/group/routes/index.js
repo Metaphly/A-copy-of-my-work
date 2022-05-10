1c99011f867fe678ce8d6d32ff9d41d0a64455e5
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-05-10 09:48:16 +0000

    Tue, May 10, 2022, 9:48 AM +00:00

--- wdc/group/routes/index.js
+++ wdc/group/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('main', { title: 'Theoparty' });
+  res.render('main.html', { title: 'Theoparty' });
 });
 
 module.exports = router;
