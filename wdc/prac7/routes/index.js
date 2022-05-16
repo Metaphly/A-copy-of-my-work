9173f42239f4844f17f6b08a8c4876139cc217b1
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-05-16 02:44:09 +0000

    Mon, May 16, 2022, 2:44 AM +00:00

---
+++ wdc/prac7/routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/brew', function(req, res, next) {
+  var reqdrink = req.query.drink;
+  if(reqdrink == "tea")
+  {
+    res.send("A delicious cup of tea!");
+  }else if(reqdrink == "coffee")
+  {
+    res.sendStatus(418);
+  }else
+  {
+    res.sendStatus(400);
+  }
+
+});
+
+let lastpass = "first";
+
+router.post('/pass-it-on', function(req, res, next) {
+  if(req.body.message)
+  {
+    res.send(lastpass);
+    lastpass = req.body.message;
+  }else
+  {
+    res.sendStatus(400);
+  }
+});
+
+router.post('/combine', function(req, res, next) {
+  let responseout = "";
+  let inputlines = req.body.lines
+  for(let line in inputlines)
+  {
+    responseout = responseout + inputlines[line] + req.body.suffix + '\n';
+  }
+  res.send(responseout);
+});
+
+module.exports = router;
