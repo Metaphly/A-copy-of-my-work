4f820d51c71eb2d84213cb23cce5a65980259370
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-05-16 09:49:23 +0000

    Mon, May 16, 2022, 9:49 AM +00:00

--- wdc/prac7/app.js
+++ wdc/prac7/app.js
@@ -8,6 +8,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var nrequest = 0;
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
