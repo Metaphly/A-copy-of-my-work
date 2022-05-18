4a5f54e13adbc6b0adc33fd11b91a7a66c59c550
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-05-18 09:58:04 +0000

    Wed, May 18, 2022, 9:58 AM +00:00

--- wdc/prac8/query1-3.sql
+++ wdc/prac8/query1-3.sql
@@ -2,4 +2,4 @@ SELECT customer.first_name,customer.last_name,rental.rental_date
 FROM rental
 INNER JOIN customer
 ON customer.customer_id = rental.customer_id
-WHERE rental.return_date = ( min(rental_date) from rental WHERE return_date is Null) ;
+WHERE rental.rental_date = (SELECT min(rental_date) from rental WHERE return_date is Null) ;
