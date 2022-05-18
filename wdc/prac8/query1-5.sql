a05b1e1551e860536569d8b18ad32d73531a0e36
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-05-18 10:20:45 +0000

    Wed, May 18, 2022, 10:20 AM +00:00

--- wdc/prac8/query1-5.sql
+++ wdc/prac8/query1-5.sql
@@ -2,6 +2,8 @@ SELECT actor_id FROM actor WHERE first_name="ANGELA" AND last_name="WITHERSPOON"
 SELECT SUM(length) FROM film;
 
 INNER JOIN customer
-ON customer.customer_id = rental.customer_id
+ON
 SELECT film_id FROM film_actor
 WHERE actor_id = (SELECT actor_id FROM actor WHERE first_name="ANGELA" AND last_name="WITHERSPOON");
+
+SELECT SUM(length) FROM film;
