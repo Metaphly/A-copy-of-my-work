318d5c8daa350a9b1e1974b1c792d21cf083c8c5
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-05-18 09:20:57 +0000

    Wed, May 18, 2022, 9:20 AM +00:00

--- wdc/prac8/query1-4.sql
+++ wdc/prac8/query1-4.sql
@@ -1,4 +1,4 @@
-SELECT customer.first_name,customer.last_name,rental.rental_date
+SELECT customer.first_name,customer.last_name,MIN(rental.rental_date)
 FROM rental
 INNER JOIN customer
 ON customer.customer_id = rental.customer_id
