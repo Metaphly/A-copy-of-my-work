861b9ed08e4c9910de03bd92fd2075eeef7c7ea7
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-05-18 10:23:10 +0000

    Wed, May 18, 2022, 10:23 AM +00:00

--- wdc/prac8/query1-5.sql
+++ wdc/prac8/query1-5.sql
@@ -1,6 +1,6 @@
 SELECT actor_id FROM actor WHERE first_name="ANGELA" AND last_name="WITHERSPOON";
 
-SELECT SUM(length) FROM film
+SELECT SUM(length FROM film
 WHERE film_id IN
 (SELECT film_id FROM film_actor
 WHERE actor_id = (SELECT actor_id FROM actor WHERE first_name="ANGELA" AND last_name="WITHERSPOON"));
