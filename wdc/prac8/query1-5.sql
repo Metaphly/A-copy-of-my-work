dc4eb58937fc36dc9c45846b8b27facd05ff3da7
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-05-18 10:22:32 +0000

    Wed, May 18, 2022, 10:22 AM +00:00

--- wdc/prac8/query1-5.sql
+++ wdc/prac8/query1-5.sql
@@ -1,9 +1,8 @@
 SELECT actor_id FROM actor WHERE first_name="ANGELA" AND last_name="WITHERSPOON";
-SELECT SUM(length) FROM film;
 
-INNER JOIN customer
-ON
-SELECT film_id FROM film_actor
-WHERE actor_id = (SELECT actor_id FROM actor WHERE first_name="ANGELA" AND last_name="WITHERSPOON");
+SELECT SUM(length) FROM film
+WHERE film_id IN
+(SELECT film_id FROM film_actor
+WHERE actor_id = (SELECT actor_id FROM actor WHERE first_name="ANGELA" AND last_name="WITHERSPOON"));
 
 SELECT SUM(length) FROM film;
