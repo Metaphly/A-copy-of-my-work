ce39eb2d923cb6b43aee0177f705281894f8338d
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-05-19 03:28:58 +0000

    Thu, May 19, 2022, 3:28 AM +00:00

--- wdc/prac8/routes/index.js
+++ wdc/prac8/routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/actors', function(req, res, next) {
+  req.pool.getConnection(function(error,connection){
+    if(error){
+      res.sendStatus(500);
+      return;
+    }
+
+    let query = "SELECT  FROM posts;";
+    connection.query(query, function(error, rows, fields) {
+      connection.release(); // release connection
+      if (error) {
+        console.log(error);
+        res.sendStatus(500);
+        return;
+      }
+      res.json(rows); //send response
+    });
+});
+
 module.exports = router;
