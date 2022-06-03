d0038e72b506ca2b66326428a29bdd1f74ed6aa4
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-03 13:32:46 +0000

    Fri, Jun 3, 2022, 1:32 PM +00:00

--- group2/routes/index.js
+++ group2/routes/index.js
@@ -9,11 +9,11 @@ router.get('/', function(req, res, next) {
 router.get('/events', function(req, res, next) {
   req.pool.getConnection(function(error,connection){
     if(error){
-      res.sendStatus(508);
+      res.sendStatus(500);
       return;
     }
 
-    let query = "SELECT event_id,event_name,location,start_time FROM events;";
+    let query = "SELECT * FROM events;";
     connection.query(query, function(error, rows, fields) {
       connection.release();
       if (error) {
