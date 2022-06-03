19a4ddb9721647bf3920effd30e109e7a85e3a6a
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-03 12:49:43 +0000

    Fri, Jun 3, 2022, 12:49 PM +00:00

--- wdc/group/public/javascripts/mainPage.js
+++ wdc/group/public/javascripts/mainPage.js
@@ -21,22 +21,3 @@ function get_events(){
     xhttp.open("GET", "/events");
     xhttp.send();
 }
-
-function send_actor()
-{
-    let fname = document.getElementById('actor-first-name').value;
-    let lname = document.getElementById('actor-last-name').value;
-    let newActor = {fname: fname, lname: lname};
-
-    let xhttp = new XMLHttpRequest();
-
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-
-        }
-    };
-
-    xhttp.open("POST", "/newActor");
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(JSON.stringify(newActor));
-}
--- wdc/group/routes/index.js
+++ wdc/group/routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/events', function(req, res, next) {
+  req.pool.getConnection(function(error,connection){
+    if(error){
+      res.sendStatus(500);
+      return;
+    }
+
+    let query = "SELECT last_name,first_name FROM actor;";
+    connection.query(query, function(error, rows, fields) {
+      connection.release();
+      if (error) {
+        res.sendStatus(500);
+        return;
+      }
+      res.json(rows);
+    });
+  });
+});
+
 module.exports = router;
