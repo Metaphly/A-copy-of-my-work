d11d3573dd36c66a1b170d438a02a14d6fab6ecf
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-03 12:23:06 +0000

    Fri, Jun 3, 2022, 12:23 PM +00:00

---
+++ wdc/group/public/javascripts/mainPage.js
@@ -0,0 +1,45 @@
+function get_events(){
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var actor_list = JSON.parse(this.responseText);
+
+            var actorArea = document.getElementById('actorlist');
+            for (let actor of actor_list) {
+                let fname = document.createElement('td');
+                let lname = document.createElement('td');
+                let wname = document.createElement('tr');
+                fname.innerText = actor.first_name;
+                lname.innerText = actor.last_name;
+                wname.appendChild(fname);
+                wname.appendChild(lname);
+                actorArea.appendChild(wname);
+            }
+        }
+    };
+    xhttp.open("GET", "/events");
+    xhttp.send();
+}
+
+function send_actor()
+{
+    let fname = document.getElementById('actor-first-name').value;
+    let lname = document.getElementById('actor-last-name').value;
+    let newActor = {fname: fname, lname: lname};
+
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+        }
+    };
+
+    xhttp.open("POST", "/newActor");
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify(newActor));
+}
+
+
+get_events();
