2223febe28a3c4e7dcdff470b3ed49c334f58124
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-03 13:10:04 +0000

    Fri, Jun 3, 2022, 1:10 PM +00:00

--- group2/routes/index.js
+++ group2/routes/index.js
@@ -13,7 +13,7 @@ router.get('/events', function(req, res, next) {
       return;
     }
 
-    let query = "SELECT last_name,first_name FROM actor;";
+    let query = "SELECT event_id,event_name,location,start_time FROM events;";
     connection.query(query, function(error, rows, fields) {
       connection.release();
       if (error) {
