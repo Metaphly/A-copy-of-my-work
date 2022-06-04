0933653807362f3cfd12db89b4e52b825166f238
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-04 10:40:26 +0000

    Sat, Jun 4, 2022, 10:40 AM +00:00

--- group2/public/javascripts/mainPage.js
+++ group2/public/javascripts/mainPage.js
@@ -48,9 +48,9 @@ function login() {
 
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            alert("Login Successful");
+            alert("Sucess");
         } else if (this.readyState == 4 && this.status >= 400) {
-            alert("Login Failed");
+            alert("Failed");
         }
     };
 
