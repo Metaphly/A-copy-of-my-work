be28851bea0517d9d367c2a883bd17128c360dd5
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-04 14:12:44 +0000

    Sat, Jun 4, 2022, 2:12 PM +00:00

--- group2/app.js
+++ group2/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var logger = require('morgan');
 var mysql = require('mysql');
 
@@ -19,6 +20,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+  secret: 'psychoanalysis',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
