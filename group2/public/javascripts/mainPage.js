46c587f870d7b9bf06824eb47d07910ee3bea216
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-04 10:34:29 +0000

    Sat, Jun 4, 2022, 10:34 AM +00:00

--- group2/public/javascripts/mainPage.js
+++ group2/public/javascripts/mainPage.js
@@ -38,6 +38,27 @@ function get_events(){
     xhttp.send();
 }
 
+function login() {
 
+    let user = {
+        username: document.getElementsByName('username')[0].value,
+        password: document.getElementsByName('password')[0].value
+    };
+
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            alert("Login Successful");
+        } else if (this.readyState == 4 && this.status >= 400) {
+            alert("Login Failed");
+        }
+    };
+
+    xhttp.open("POST", "/login");
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify(user));
+
+}
 
 get_events();
