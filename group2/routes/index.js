3ad5ea84ddc60d101c45f892da97dd99a83c31d1
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-04 10:43:40 +0000

    Sat, Jun 4, 2022, 10:43 AM +00:00

--- group2/routes/index.js
+++ group2/routes/index.js
@@ -43,4 +43,23 @@ router.post('/login', function(req, res, next) {
 
 });
 
+router.get('/login', function(req, res, next) {
+  req.pool.getConnection(function(error,connection){
+    if(error){
+      res.sendStatus(500);
+      return;
+    }
+
+    let query = "SELECT event_id,event_name,location,start_time FROM events;";
+    connection.query(query, function(error, rows, fields) {
+      connection.release();
+      if (error) {
+        res.sendStatus(500);
+        return;
+      }
+      res.json(rows);
+    });
+  });
+});
+
 module.exports = router;
