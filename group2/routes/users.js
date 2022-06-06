73eb2fd5e192fa3ad6c415bf014af1836afa19c8
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-06 10:21:43 +0000

    Mon, Jun 6, 2022, 10:21 AM +00:00

--- group2/routes/users.js
+++ group2/routes/users.js
@@ -13,6 +13,16 @@ router.use('/', function(req, res, next) {
   }
 });
 
+router.use('/userPage', function(req, res, next) {
+  if(!(user in req.session)) {
+    console.log("Haven't login");
+    res.sendStatus(403);
+  }else
+  {
+    next();
+  }
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
