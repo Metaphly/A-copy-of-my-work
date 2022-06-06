eefd8fa04a45f6d06d38dd0b39e4ec0662e40281
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-06 10:09:39 +0000

    Mon, Jun 6, 2022, 10:09 AM +00:00

--- group2/routes/index.js
+++ group2/routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/loginPage', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public/login.html'));
+  res.sendFile(path.join(__dirname, './public/login.html'));
 });
 
 router.get('/events', function(req, res, next) {
