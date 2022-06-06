53c62bfee34b0bd9a9cb5140ae90528555fab799
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-06 10:09:01 +0000

    Mon, Jun 6, 2022, 10:09 AM +00:00

--- group2/app.js
+++ group2/app.js
@@ -28,10 +28,6 @@ app.use(session({
   cookie: { secure: false }
 }));
 
-app.get('/loginPage', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public/login.html'));
-});
-
 app.get('/signupPage', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/signup.html'));
 });
--- group2/routes/index.js
+++ group2/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/loginPage', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/login.html'));
 });
 
 router.get('/events', function(req, res, next) {
