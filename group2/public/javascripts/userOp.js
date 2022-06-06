d79532b32ea832c2998292031c1fb3e68b68ec58
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-06 12:44:23 +0000

    Mon, Jun 6, 2022, 12:44 PM +00:00

--- group2/public/javascripts/userOp.js
+++ group2/public/javascripts/userOp.js
@@ -5,7 +5,7 @@ function get_userInfo(){
         if (this.readyState == 4 && this.status == 200) {
             var userinfo = JSON.parse(this.responseText);
             var infoArea = document.getElementById('userInfo');
-            
+
             let infotable = document.createElement('table');
             let infotitles = document.createElement('tr');
             infotitles.innerHTML = "<th>User Id</th> <th>User Name</th>";
@@ -13,7 +13,7 @@ function get_userInfo(){
 
             let detailrow = document.createElement('tr');
             let user_id = document.createElement('th');
-            let user_email = document.createElement('th');
+            let user_name = document.createElement('th');
             user_id.innerText = userinfo.user_id;
             user_email.innerText = userinfo.email;
 
