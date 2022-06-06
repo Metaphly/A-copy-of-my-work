8fef457d97f6f2ef86182396e88cad3653fc5b0f
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-06 09:59:50 +0000

    Mon, Jun 6, 2022, 9:59 AM +00:00

--- group2/app.js
+++ group2/app.js
@@ -29,7 +29,7 @@ app.use(session({
 }));
 
 app.get('/loginPage', function(req, res) {
-  res.sendFile(path.join(__dirname, '/login.html'));
+  res.sendFile(path.join(__dirname, 'public/login.html'));
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
