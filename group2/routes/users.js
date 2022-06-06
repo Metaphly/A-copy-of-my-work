4c09804b93279d2c689bcd3b5189d317fd16319f
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-06 13:29:13 +0000

    Mon, Jun 6, 2022, 1:29 PM +00:00

--- group2/routes/users.js
+++ group2/routes/users.js
@@ -48,4 +48,36 @@ router.get('/userInfo', function(req, res, next) {
   });
 });
 
+router.post('/changeEmail', function(req, res, next) {
+  req.pool.getConnection(function(error,connection){
+    if(error){
+      res.sendStatus(500);
+      return;
+    }
+
+    let query = "SELECT * FROM users WHERE user_name = ?;";
+    connection.query(query,[req.body.user_name], function(error, rows, fields) {
+      connection.release();
+      if (error) {
+        res.sendStatus(500);
+        return;
+      }
+
+      if(rows.length==0)
+      {
+        console.log('incorrect email');
+        res.sendStatus(401);
+      } else if(rows[0].password == req.body.password) {
+        console.log('sccuess');
+        req.session.user = rows[0];
+        res.sendStatus(200);
+      } else {
+        console.log('wrong password');
+        res.sendStatus(401);
+      }
+
+    });
+  });
+});
+
 module.exports = router;
