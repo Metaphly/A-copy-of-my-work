1d5fd434780900ef3d7959a4fd69e55407e4981b
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-07 03:08:44 +0000

    Tue, Jun 7, 2022, 3:08 AM +00:00

--- group2/event.sql
+++ group2/event.sql
@@ -12,8 +12,8 @@ CREATE TABLE events
     event_id int NOT NULL AUTO_INCREMENT,
     event_name VARCHAR(60),
     location VARCHAR(60),
-    start_time VARCHAR(60),
-    end_time VARCHAR(60),
+    start_date VARCHAR(60),
+    final_time VARCHAR(60),
     description VARCHAR(400),
     creator int,
     PRIMARY KEY (event_id),
@@ -25,6 +25,7 @@ CREATE TABLE user_events
     id int NOT NULL AUTO_INCREMENT,
     user_id int,
     event_id int,
+    free_time int,
     PRIMARY KEY (id,user_id,event_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
     FOREIGN KEY (event_id) REFERENCES events(event_id) ON DELETE CASCADE
