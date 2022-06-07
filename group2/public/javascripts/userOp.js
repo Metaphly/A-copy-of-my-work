4c0a8fb5046e660af76c60aa642e67d821b357cc
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-07 11:19:28 +0000

    Tue, Jun 7, 2022, 11:19 AM +00:00

--- group2/public/javascripts/userOp.js
+++ group2/public/javascripts/userOp.js
@@ -137,7 +137,9 @@ function get_myevents(){
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
+            element = document.getElementsByTagName("select");
 
+            
         }
     };
     xhttp.open("GET", "/users/myevents");
