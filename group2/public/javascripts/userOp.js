c70759bb65ba5c1eff0ff6efa550dd66d8ff9776
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-07 01:21:48 +0000

    Tue, Jun 7, 2022, 1:21 AM +00:00

--- group2/public/javascripts/userOp.js
+++ group2/public/javascripts/userOp.js
@@ -34,9 +34,9 @@ function change_email(){
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-
+            
         }
     };
-    xhttp.open("GET", "/users/");
+    xhttp.open("GET", "/users/changeEmail");
     xhttp.send();
 }
