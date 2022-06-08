e850c876d63aa5d79fc0ab72d87cc16e5380bc26
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-08 10:34:21 +0000

    Wed, Jun 8, 2022, 10:34 AM +00:00

--- group2/routes/index.js
+++ group2/routes/index.js
@@ -157,15 +157,11 @@ router.post('/googleuser', function(req, res, next) {
 
   async function verify() {
     const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-        // Or, if multiple clients access the backend:
-        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+        idToken: req.body.token,
+        audience: CLIENT_ID,
     });
     const payload = ticket.getPayload();
     const userid = payload['sub'];
-    // If request specified a G Suite domain:
-    // const domain = payload['hd'];
   }
   verify().catch(console.error);
 
