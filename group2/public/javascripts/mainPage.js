6109baa0244a882838e7db2baa563c7476409554
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-09 02:27:04 +0000

    Thu, Jun 9, 2022, 2:27 AM +00:00

--- group2/public/javascripts/mainPage.js
+++ group2/public/javascripts/mainPage.js
@@ -179,3 +179,15 @@ function signOut() {
 }
 
 // sign out google if no user session
+function signOut_google(){
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            
+            }
+        }
+    };
+    xhttp.open("GET", "/events");
+    xhttp.send();
+}
