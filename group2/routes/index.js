cdf410c220b64953b40e304fd0443a5ec06ed07c
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-09 14:01:48 +0000

    Thu, Jun 9, 2022, 2:01 PM +00:00

--- group2/routes/index.js
+++ group2/routes/index.js
@@ -142,6 +142,13 @@ router.post('/signup', function(req, res, next) {
       req.session.user = {"user_name":req.body.user_name, "email":""};
     });
 
+    console.log("exist again");
+    if(exist)
+    {
+      console.log("work here");
+      return;
+    }
+
     // query user info and store in session
     connection.query("SELECT * FROM users WHERE user_name = ?;",[req.body.user_name], function(error, rows, fields) {
       connection.release();
