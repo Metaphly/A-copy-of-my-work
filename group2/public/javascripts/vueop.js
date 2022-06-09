15a41fb93f8e98543bbedb0460dc39068da3ae07
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-09 00:53:56 +0000

    Thu, Jun 9, 2022, 12:53 AM +00:00

--- group2/public/javascripts/vueop.js
+++ group2/public/javascripts/vueop.js
@@ -2,7 +2,8 @@ var part = new Vue({
     el: '#notblue',
     data: {
       show_info: true,
-      show_event: true
+      show_event: true,
+      button:"Hide Penal"
     },
     methods: {
         hideShow: function () {
