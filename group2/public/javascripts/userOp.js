1deeb4e8262046f238c0c21eda668c9a93d36720
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-09 02:16:11 +0000

    Thu, Jun 9, 2022, 2:16 AM +00:00

--- group2/public/javascripts/userOp.js
+++ group2/public/javascripts/userOp.js
@@ -342,11 +342,3 @@ function set_finaltime() {
     xhttp.send(JSON.stringify(user_event));
 
 }
-
-// google sign out
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-}
