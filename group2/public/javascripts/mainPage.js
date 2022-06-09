5f22e330014dde1c8333a81acbf2169cc0f7bea0
Metaphly <100544612+Metaphly@users.noreply.github.com>
2022-06-09 02:04:59 +0000

    Thu, Jun 9, 2022, 2:04 AM +00:00

--- group2/public/javascripts/mainPage.js
+++ group2/public/javascripts/mainPage.js
@@ -169,3 +169,11 @@ function onSignIn(googleUser) {
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send(JSON.stringify(googleuser));
 }
+
+// google log out
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+}
